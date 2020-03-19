5e76c4978c2dcf315a3eb9d6b6f03b4877e74210
Daniel Sun <danielsun12345@gmail.com>
2020-03-19 20:21:39 +0000

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT, 
+  author_id INTEGER, 
+  sub_genre INTEGER
+);
+
+CREATE TABLE sub_genres (
+  id INTEGER PRIMARY KEY, 
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT 
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year TEXT,
+  series_id INTEGER
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT, 
+  species TEXT,
+  author_id INTEGER 
+);
+
+CREATE TABLE character_books (
+  
+);
