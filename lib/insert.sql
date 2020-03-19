768c2b8917dc5aa311ed3f4a847f9f392e79f3b3
Daniel Sun <danielsun12345@gmail.com>
2020-03-19 20:33:27 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,32 +1,32 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES 
 (1, "A Song of Ice and Fire", 1, 1),
-(2, "Second Series", 2, 2);
+(2, "Harry Potter", 2, 2);
 
 INSERT INTO subgenres (id, name) VALUES 
 (1, "medieval"), 
-(2, "space opera");
+(2, "Sci-Fi");
 
 INSERT INTO authors (id, name) VALUES 
 (1, "George R. R. Martin"),
-(2, "Second Author");
+(2, "J. K. Rowling");
 
 INSERT INTO books (id, title, year, series_id) VALUES 
 (1, "Game of Thrones", 1996, 1), 
 (2, "A Clash of Kings", 1998, 1), 
 (3, "A Storm of Swords", 2000, 1), 
-(4, "First Book", 2002, 2), 
-(5, "Second Book", 2003, 2), 
-(6, "Third Book", 2005, 2);
+(4, "The Philosophers Stone", 1997, 2), 
+(5, "The Chamber of Secrets", 1998, 2), 
+(6, "Prisoner of Azkaban", 2000, 2);
 
 INSERT INTO characters (id, name, motto, species, author_id) VALUES 
 (1, "Lady", "Woof Woof", "direwolf", 1), 
 (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1), 
 (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1), 
 (4, "Eddard Stark", "Winter is coming", "human", 1),
-(5, "Character One", "motto one", "cylon", 2), 
-(6, "Character Two", "motto two", "human", 2), 
-(7, "Character Three", "motto three", "cylon", 2), 
-(8, "Character Four", "motto four", "cylon", 2);
+(5, "Harry Potter", "Rictusempra", "human", 2), 
+(6, "Draco Malfoy", "Petrificus Totalus", "human", 2), 
+(7, "Ron Weasley", "Sectumsempra", "human", 2), 
+(8, "Hermione Granger", "Crucio", "human", 2);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES 
 (1, 1, 1), 
