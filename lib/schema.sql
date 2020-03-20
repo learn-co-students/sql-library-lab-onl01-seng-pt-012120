5e28295600ebb84ea954aa898f5e0de6df07321e
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-20 18:12:55 +0000

    Done.

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,24 +1,31 @@
-create Table Series (
+create Table series (
 id INTEGER PRIMARY KEY,
 title TEXT,
-author_id INTEGER); 
+author_id INTEGER,
+subgenre_id INTEGER); 
 
-create Table Sub-Genres (
+create Table subgenres (
 id INTEGER PRIMARY KEY,
 name TEXT);
 
-create Table Authors (
+create Table authors (
 id INTEGER PRIMARY KEY,
 name TEXT); 
 
-create Table Books ( 
+create Table books ( 
 id INTEGER PRIMARY KEY,
 title TEXT,
 year INTEGER, 
 series_id INTEGER); 
 
+create Table characters (
+id INTEGER PRIMARY KEY, 
+name TEXT, 
+motto TEXT, 
+species TEXT,
+author_id INTEGER);
+
 create Table character_books (
-  id INTEGER PRIMARY KEY
-  character_id INTEGER 
-  book_id INTEGER
-)
+  id INTEGER PRIMARY KEY,
+  character_id INTEGER, 
+  book_id INTEGER);
