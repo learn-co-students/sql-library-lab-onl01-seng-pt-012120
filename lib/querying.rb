9a1e4e7612e4c75f4f1b8f462db06e323f41ea06
Derric Fragosa <fderric@gmail.com>
2020-03-31 21:50:06 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,9 +15,11 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species 
+  "SELECT species,
+  COUNT (species) AS most_species
   FROM characters
-  ORDER BY COUNT(species) ;"
+  GROUP BY species
+  ORDER BY  most_species DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
